626a0a416f23bcfe6fbfa17b5c2884610be7fb49
Naveen <therealnaveenkamal@gmail.com>
2023-10-06 12:13:33 +0000

    Completes multiple laps

--- robot_patrol/src/patrol_ext.cpp
+++ robot_patrol/src/patrol_ext.cpp
@@ -13,14 +13,14 @@ public:
         "scan", 10,
         std::bind(&Patrol::laserCallback, this, std::placeholders::_1));
     vPubs = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    oth = 0.6;
+    oth = 0.4;
     cTimer = create_wall_timer(125ms, std::bind(&Patrol::controlLoop, this));
   }
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lSubs;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vPubs;
-  double oth = 0.6;
+  double oth;
   rclcpp::TimerBase::SharedPtr cTimer;
   geometry_msgs::msg::Twist velocity;
 
@@ -29,32 +29,27 @@ private:
     double left = msg->ranges[180];
     double right = msg->ranges[540];
 
-    if (front < oth) {
-      if (right > oth || left > oth) {
-        RCLCPP_INFO(this->get_logger(), "Turning Right: '%f'", right);
-
-        if (std::isinf(right)) {
-          RCLCPP_INFO(this->get_logger(), "Reverse: '%f'", right);
-          velocity.linear.x = 0;
-          velocity.angular.z = -0.5;
-          vPubs->publish(velocity);
-        }
-
-        velocity.linear.x = 0;
-        velocity.angular.z = -0.5;
+    RCLCPP_INFO(this->get_logger(), "Front: '%f', Left: '%f', Right: '%f' ",
+                front, left, right);
+    if (front < oth || std::isinf(front)) {
+      if (left > oth && left > right) {
+        RCLCPP_INFO(this->get_logger(), "Left: '%f', Right: '%f'", left, right);
+        velocity.linear.x = -0.1;
+        velocity.angular.z = 0.5;
         vPubs->publish(velocity);
-      } else if (right < 0.15) {
+      } else if (right > oth) {
+        RCLCPP_INFO(this->get_logger(), "Turning Right: '%f'", right);
         velocity.linear.x = -0.1;
         velocity.angular.z = -0.5;
         vPubs->publish(velocity);
       } else {
         RCLCPP_INFO(this->get_logger(), "Reverse: '%f'", front);
-        velocity.linear.x = -0.1;
-        velocity.angular.z = -0.1;
+        velocity.linear.x = -0.3;
+        velocity.angular.z = 0.0;
         vPubs->publish(velocity);
       }
     } else {
-      velocity.linear.x = 0.2;
+      velocity.linear.x = 0.3;
       velocity.angular.z = 0.0;
       vPubs->publish(velocity);
     }
