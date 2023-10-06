c1d847400e15a8166f3b756e2e5103a12052f57f
Naveen <therealnaveenkamal@gmail.com>
2023-10-06 04:03:12 +0000

    Forward and Backward stagnant movement rectified

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,6 +1,5 @@
 #include <chrono>
 #include <geometry_msgs/msg/twist.hpp>
-#include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 
@@ -9,44 +8,51 @@ using namespace std::chrono_literals;
 class Patrol : public rclcpp::Node {
 public:
   Patrol() : Node("patrol") {
-    lSubs = this->create_subscription<sensor_msgs::msg::LaserScan>(
+    lSubs = create_subscription<sensor_msgs::msg::LaserScan>(
         "scan", 10,
         std::bind(&Patrol::laserCallback, this, std::placeholders::_1));
-    vPubs = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    oth = 0.5;
-    cTimer = this->create_wall_timer(std::chrono::milliseconds(125),
-                                     std::bind(&Patrol::controlLoop, this));
-    rev = false;
+    vPubs = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    oth = 0.4;
+    cTimer = create_wall_timer(125ms, std::bind(&Patrol::controlLoop, this));
   }
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lSubs;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vPubs;
-  double oth = 0.45;
+  double oth;
   rclcpp::TimerBase::SharedPtr cTimer;
-  bool rev;
+  geometry_msgs::msg::Twist velocity;
+
   void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    double front_range = msg->ranges[360];
-    if (front_range < oth) {
-      rev = true;
+    double front = msg->ranges[360];
+    double left = msg->ranges[180];
+    double right = msg->ranges[540];
+
+    if (front < oth) {
+      if (left > oth) {
+        RCLCPP_INFO(this->get_logger(), "Turning Left: '%f'", left);
+        velocity.linear.x = -0.1;
+        velocity.angular.z = 0.1;
+        vPubs->publish(velocity);
+      } else if (right > oth) {
+        RCLCPP_INFO(this->get_logger(), "Turning Right: '%f'", right);
+        velocity.linear.x = -0.1;
+        velocity.angular.z = -0.1;
+        vPubs->publish(velocity);
+      } else {
+        RCLCPP_INFO(this->get_logger(), "Reverse: '%f'", front);
+        velocity.linear.x = -0.1;
+        velocity.angular.z = -0.1;
+        vPubs->publish(velocity);
+      }
     } else {
-      rev = false;
+      velocity.linear.x = 0.1;
+      velocity.angular.z = 0.0;
+      vPubs->publish(velocity);
     }
   }
 
-  void controlLoop() {
-    geometry_msgs::msg::Twist velocity_command;
-
-    if (rev) {
-      velocity_command.linear.x = -0.1;
-      velocity_command.angular.z = 0.0;
-    } else {
-      velocity_command.linear.x = 0.1;
-      velocity_command.angular.z = 0.0;
-    }
-
-    vPubs->publish(velocity_command);
-  }
+  void controlLoop() {}
 };
 
 int main(int argc, char *argv[]) {
